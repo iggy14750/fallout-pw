a2e97083e1c02b7e17d74c85595c1191949dfa42
iggy14750 <ibg4@pitt.edu>
2018-01-17 17:34:13 -0500

    basic file in place.

---
+++ fallout.py
@@ -0,0 +1,7 @@
+
+""" A utility for cracking terminals in the Fallout universe. """
+
+class PasswordSet:
+    """ The primary abstraction, PasswordSet holds, shocking,
+    a set of passwords, and provides methods for querying them. """
+    pass
