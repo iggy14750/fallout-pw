e40c0b8ccb6a0541a326e24edff3973e2fc31069
iggy14750 <ibg4@pitt.edu>
2018-01-17 20:21:37 -0500

    removed PasswordSet code

--- fallout.py
+++ fallout.py
@@ -35,27 +35,5 @@ def filter_shares(iterable, word, number):
     )
 
 
-
-
-
-
-
-
-class PasswordSet:
-    """ The primary abstraction, PasswordSet holds, shocking,
-    a set of passwords, and provides methods for querying them. """
-
-    def __init__(self, passwords):
-        self._words = list(passwords)
-
-    def __iter__(self):
-        return iter(self._words)
-
-    def best(self):
-        """ Returns the best choice out these possible passwords.
-        Determined as the one which shares the most letters with the others. """
-        return self._words[0]
-
-
 if __name__ == '__main__':
     pass
--- test_fallout.py
+++ test_fallout.py
@@ -53,40 +53,5 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual([], list(fallout.filter_shares(iterable, word, 3)))
 
 
-
-
-
-
-
-
-
-class TestPasswordSet(unittest.TestCase):
-    """ Pretty self-explanatory. """
-    def test_constructor(self):
-        """For lint"""
-        self.assertIsNotNone(fallout.PasswordSet(
-            ['One', 'Two', 'Three']
-        ))
-
-    def test_iterator(self):
-        """ Test that this object outputs the same objects in an iterator."""
-        words = {'one', 'two', 'three'}
-        self.assertEqual(words, set(fallout.PasswordSet(words)))
-
-    def test_best_single_element(self):
-        """ Initializing a PasswordSet with a single element will make that element the best. """
-        self.assertEqual('single', fallout.PasswordSet(['single']).best())
-
-    def test_best_first_element(self):
-        """ The best of this list happens to be the first one. """
-        words = ['fire', 'ford', 'kirk']
-        self.assertEqual(words[0], fallout.PasswordSet(words).best())
-
-    def _test_best_not_first_element(self):
-        """ Now the best element is the second one. """
-        words = ['rimir', 'rumor', 'yuyoy']
-        self.assertEqual(words[1], fallout.PasswordSet(words).best())
-
-
 if __name__ == '__main__':
     unittest.main()
